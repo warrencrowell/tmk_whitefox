89228e7bf8af1badc8a2f8107c25ab150aef6705
Warren Crowell <wmc62@cornell.edu>
2019-02-23 14:29:34 -0500

    Adding current whitefox mapping

--- keymap_plain.c
+++ keymap_plain.c
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "keymap_common.h"
+#include "led_controller.h"
 
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Default Layer
@@ -31,21 +32,48 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------------------------------------'
      */
     [0] = KEYMAP( \
-        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV, INS, \
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,     DEL, \
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,NUHS,ENT,      PGUP,\
-        LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,     UP,  PGDN,\
+        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,BSPC, GRV, \
+        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,     DEL, \
+        LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,NUHS,ENT,      HOME,\
+        LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,     UP,  END,\
         LCTL,LGUI,LALT,               SPC,           RALT,FN0, RCTL,     LEFT,DOWN,RGHT \
     ),
     [1] = KEYMAP( \
         TRNS,F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS,TRNS,MUTE,\
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS,TRNS,     TRNS,\
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     VOLU,\
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     PGUP,VOLD,\
-        TRNS,TRNS,TRNS,               TRNS,          TRNS,TRNS,TRNS,     HOME,PGDN,END  \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,MRWD,MFFD,MSTP,     INS,\
+        TRNS, FN5,TRNS,TRNS,TRNS, FN4,TRNS,TRNS,TRNS,TRNS,MPRV,MNXT,   0,MPLY,     PGUP,\
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, FN2,FN1,FN3,TRNS,     VOLU,PGDN,\
+        TRNS,TRNS,TRNS,               TRNS,          TRNS,TRNS,MUTE,     HOME,VOLD,END  \
     ),
 };
+#define ACTION_LEDS_GAME 0
+#define ACTION_LEDS_ALL 1
 
 const action_t fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
+    [1] = ACTION_BACKLIGHT_INCREASE(),
+    [2] = ACTION_BACKLIGHT_DECREASE(),
+    [3] = ACTION_BACKLIGHT_TOGGLE(),
+    [4] = ACTION_FUNCTION(ACTION_LEDS_GAME),
+    [5] = ACTION_FUNCTION(ACTION_LEDS_ALL),
+
 };
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt){
+    (void)opt;
+    switch(id) {
+        case ACTION_LEDS_GAME:
+            // toggle on press
+            if(record->event.pressed){
+                chMBPost(&led_mailbox, LED_MSG_GAME_TOGGLE, TIME_IMMEDIATE);
+            }
+            break;
+        case ACTION_LEDS_ALL:
+            // toggle on press
+            if(record->event.pressed){
+                chMBPost(&led_mailbox, LED_MSG_ALL_TOGGLE, TIME_IMMEDIATE);
+            }
+            break;
+    }
+
+}
